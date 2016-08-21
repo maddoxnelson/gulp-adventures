b825abc8f039b967895568234d01e844400429b5
Matt Nelson <maddoxnelson@gmail.com>
2016-08-20 20:07:04 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var fs = require('fs');
+var prompt = require('gulp-prompt');
+var replace = require('gulp-replace');
+var insertLines = require('gulp-insert-lines');
